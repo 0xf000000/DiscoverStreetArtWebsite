416fb09eccb8ec4cb86bfaebf9daeb53b37477a6
0xf000000 <leon.margaleaws@gmail.com>
2023-12-22 15:19:02 +0100

    cool:

---
+++ stree/src/main/java/discover/streetart/stree/main/StreeApplication.java
@@ -0,0 +1,13 @@
+package discover.streetart.stree.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreeApplication.class, args);
+	}
+
+}
---
+++ stree/src/test/java/discover/streetart/stree/StreeApplicationTests.java
@@ -0,0 +1,13 @@
+package discover.streetart.stree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

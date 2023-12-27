2dd3f7d325d155ba2b3815ba3ae32f78ff291317
0xf000000 <Leon@margale.de>
2023-12-27 13:45:57 +0100

    first setup of spring security done

--- backend/src/main/java/discover/streetart/main/security/SecurityConfiguration.java
+++ backend/src/main/java/discover/streetart/main/security/SecurityConfiguration.java
@@ -1,52 +1,32 @@
 package discover.streetart.main.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
-import javax.sql.DataSource;
-
 
 @Configuration
 public class SecurityConfiguration {
 
-    DataSource dataSource;
-    public SecurityConfiguration( DataSource dataSource){
-        this.dataSource = dataSource;
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth)
-            throws Exception {
-        auth.jdbcAuthentication()
-                .dataSource(dataSource)
-                .passwordEncoder(NoOpPasswordEncoder.getInstance())
-                .usersByUsernameQuery("select username, password, active  AS enabled from usr where username=?")
-                .authoritiesByUsernameQuery("select u.username, ur.roles AS authority from usr u inner join user_role ur on u.id = ur.user_id where u.username=?");
-    }
-
 
     @Bean
-    public InMemoryUserDetailsManager userDetailsService(){
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("foo")
-                .password("foo")
+    public InMemoryUserDetailsManager UserDetailsService(){
+       PasswordEncoder encoder = passwordEncoder();
+        UserDetails user = User.withUsername("foo")
+                .password(encoder.encode("foo"))
                 .roles("USER")
                 .build();
 
         return new InMemoryUserDetailsManager(user);
 
     }
-
-
-
-
 
 
 }
